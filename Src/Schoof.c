e6c0e649f85e2c703aa03149545ea1c7b6d748f4
Boukha98 <45970119+Boukha98@users.noreply.github.com>
2019-01-19 21:54:36 +0100

    Add files via upload

--- Src/Schoof.c
+++ Src/Schoof.c
@@ -15,22 +15,28 @@ void Poly_division(int m, fq_t a, fq_t b, fq_t X, fq_t Y, fq_ctx_t ctx){
 	int n=floor(m/2); int i=0;
 	fq_t Psi[m+1];
 	
+	/* Initialisation des variables */
 	fq_init(X2,ctx);fq_init(X4,ctx); fq_init(X6,ctx);fq_init(a2,ctx); 
 	fq_init(a3, ctx);fq_init(b2,ctx);fq_init(c1,ctx);fq_init(c2,ctx);
 	fq_init(c3,ctx);fq_init(c4,ctx);fq_init(c5,ctx); 
 	for(i=0;i<=m+1;i++){fq_init(Psi[i],ctx);}
 	
+	/* Assignation des valeurs X2=X*X,X4=X2*X2... */
 	fq_sqr(X2,X,ctx);fq_sqr(X4,X2,ctx); fq_mul(X6,X4,X2,ctx);fq_sqr(a2,a,ctx);
 	fq_mul(a3,a,a2,ctx);fq_sqr(b2,b,ctx);
 	
+	/* Psi0,Psi1 */
 	fq_set_si(Psi[0],0,ctx);fq_set_si(Psi[1],1,ctx);
 	
+	/* Psi2 */
 	fq_mul_si(Psi[2],Y,2,ctx);
 	
+	/* Psi3 */
 	fq_mul_si(c1,X4,3,ctx);fq_mul(c2,a,X2,ctx);fq_mul_si(c2,c2,6,ctx);
 	fq_mul(c3,b,X,ctx);fq_mul_si(c3,c3,12,ctx);fq_set(c4,a2,ctx);
 	fq_add(c1,c1,c2,ctx); fq_sub(c3,c3,c4,ctx);fq_add(Psi[3],c1,c3,ctx);
 	
+	/* Psi4 */
 	fq_mul(c1,a,X4,ctx);fq_mul_si(c1,c1,5,ctx);fq_mul(c2,b,X2,ctx);
 	fq_mul_si(c2,c2,20,ctx);fq_mul(c3,a2,X2,ctx);fq_mul_si(c3,c3,5,ctx);
 	fq_mul(c4,a,b,ctx);fq_mul(c4,c4,X,ctx);fq_mul_si(c4,c4,4,ctx);
@@ -38,9 +44,11 @@ void Poly_division(int m, fq_t a, fq_t b, fq_t X, fq_t Y, fq_ctx_t ctx){
 	fq_sub(c2,c2,c5,ctx);fq_sub(c2,c2,a3,ctx);fq_add(c1,c1,c2,ctx);
 	fq_add(c1,c1,c3,ctx);fq_mul(c1,c1,Y,ctx);fq_mul_si(Psi[4],c1,4,ctx);
 	
+	/* Psi5 */
 	fq_pow_ui(c1,Psi[2],3,ctx);fq_mul(c2,c1,Psi[4],ctx);fq_pow_ui(c3,Psi[3],3,ctx);
 	fq_sub(Psi[5],c2,c3,ctx);
 	
+	/* Psim, si n>=3 */
 	if(n>=3){
 		for(i=3;i<=n;i++){
 			fq_sqr(c1,Psi[n-1],ctx);fq_mul(c2,c1,Psi[n+2],ctx);fq_sqr(c3,Psi[n+1],ctx);
